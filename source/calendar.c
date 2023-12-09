fc02f5cdb1994181622b51c313ecab234ec61922
Dakuro <dakuro.dark1@gmail.com>
2023-12-09 14:31:48 +0100

    Optimize conditions

--- source/calendar.c
+++ source/calendar.c
@@ -3,18 +3,18 @@
 #include "../header/functions.h"
 
 const char *MONTH_NAMES[12] = {
-		"Janvier",
-		"Février",
-		"Mars",
-		"Avril",
-		"Mai",
-		"Juin",
-		"Juillet",
-		"Août",
-		"Septembre",
-		"Octobre",
-		"Novembre",
-		"Décembre"
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre"
 };
 
 const char *DAYS_NAMES = "lu ma me je ve sa di";
@@ -37,10 +37,7 @@ gregorianDate getDate(){
 int getMonthLength(gregorianDate date){
 	int julianDay1, julianDay2;
 	julianDay1 = toJulian(date);
-	if(date.month == 12){
-		date.month = 1;
-		date.year += 1;
-	} else{date.month += 1;}
+    date.month == 12 ? (date.month = 1, ++date.year) : ++date.month;
 	julianDay2 = toJulian(date);
 	return julianDay2 - julianDay1;
 }
@@ -59,55 +56,44 @@ void printCalendar(){
 	int dayToPrint = 1;
 	int dayNameID = getDayNameID(date);
 	printf("\n");
-	for(int i = 0; i < CALENDAR_HEIGHT; i++){
-		for(int j = 0; j < CALENDAR_WIDTH; j++){
-			switch (i) {
+	for(int i = 0; i < CALENDAR_HEIGHT; ++i){
+		for(int j = 0; j < CALENDAR_WIDTH; ++j){
+			switch(i){
 				case 0: // First line
 				case 2: // Third line
 				case 10: // Last line
-					if(j == 0 || j == CALENDAR_WIDTH - 1){
-						printf("+");
-					} else{printf("-");}
+                    printf(j == 0 || j == CALENDAR_WIDTH - 1 ? "+" : "-");
 					break;
 				case 1: // Second line
-					if(j == 0 || j == CALENDAR_WIDTH - 1){
-						printf("|");
-					} else if(j == 2){ // Display month and year
-						printf("%s%s%d", MONTH_NAMES[date.month - 1], " ", date.year);
-						if (date.month == 2 || date.month == 8 || date.month == 12) {
-							// Months with an accentuated letter in their name need different spacing
-							j += strlen(MONTH_NAMES[date.month - 1]) + 3;
-						} else {
-							j += strlen(MONTH_NAMES[date.month - 1]) + 4;
-						}
-					} else{printf(" ");}
+					if(j == 0 || j == CALENDAR_WIDTH - 1) printf("|");
+					else if(j == 2){ // Display month and year
+						printf("%s %d", MONTH_NAMES[date.month - 1], date.year);
+                        j += strlen(MONTH_NAMES[date.month - 1])
+                            + (date.month == 2 || date.month == 8 || date.month == 12 ? 3 : 4);
+					} else printf(" ");
 					break;
 				case 3: // Fourth line
-					if(j == 0 || j == CALENDAR_WIDTH - 1){
-						printf("|");
-					} else if(j == 2){ // Display names of the days
+					if(j == 0 || j == CALENDAR_WIDTH - 1) printf("|");
+					else if(j == 2){ // Display names of the days
 						printf("%s", DAYS_NAMES);
 						j += strlen(DAYS_NAMES) - 1;
-					} else{printf(" ");}
+					} else printf(" ");
 					break;
 				default: // All other lines
-					if(i > 4){dayNameID = 0;} // Reset dayNameID if day 1 has already been printed (= on fifth line)
-					if(j == 0 || j == CALENDAR_WIDTH - 1){
-						printf("|");
-					} else if(j == 2 + 3 * dayNameID && dayToPrint <= monthLength){ // Display days, if applicable
-						for(int k = dayNameID; k < 7; k++){
-							dayToPrint < 10 ? printf("%s%d", " ", dayToPrint) : printf("%d", dayToPrint);
-							printf(" ");
-							dayToPrint++;
+					if(i > 4) dayNameID = 0; // Reset dayNameID if day 1 has already been printed (= on fifth line)
+					if(j == 0 || j == CALENDAR_WIDTH - 1) printf("|");
+					else if(j == 2 + 3 * dayNameID && dayToPrint <= monthLength){ // Display days, if applicable
+						for(int k = dayNameID; k < 7; ++k){
+							dayToPrint < 10 ? printf(" %d ", dayToPrint) : printf("%d ", dayToPrint);
+							++dayToPrint;
 							j += 3;
-							if(dayToPrint > monthLength){break;}
+							if(dayToPrint > monthLength) break;
 						}
-						j--;
-					} else{printf(" ");}
+						--j;
+					} else printf(" ");
 					break;
 			}
 		}
 		printf("\n");
 	}
-
 }
