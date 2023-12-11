8aababa5be9aab2066956f84e15ff8177d8ffa6a
Dakuro <51865183+Dakuro@users.noreply.github.com>
2023-12-11 20:17:07 +0100

    Move switch to new function

--- header/functions.h
+++ header/functions.h
@@ -23,6 +23,7 @@ gregorianDate toGregorian(int julianDay); // Convert a Julian Day to a Gregorian
 gregorianDate getDate(); // Prompt user for a year and month and return the Gregorian date for day 1
 int getMonthLength(gregorianDate date); // Use a Gregorian date to get the number of days in its month and return it
 int getDayNameID(gregorianDate date); // Return an ID corresponding to the day (0 -> Monday, ..., 6 -> Sunday)
+int calendarLogic(gregorianDate date, int height, int width, int monthLength, int *dayToPrint, int dayNameID);
 void printCalendar(); // Print a calendar page
 
 // Biorhythm
--- source/calendar.c
+++ source/calendar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "../header/functions.h"
 
 const char *MONTH_NAMES[12] = {
@@ -37,7 +38,7 @@ gregorianDate getDate(){
 int getMonthLength(gregorianDate date){
 	int julianDay1, julianDay2;
 	julianDay1 = toJulian(date);
-    date.month == 12 ? (date.month = 1, ++date.year) : ++date.month;
+	date.month == 12 ? (date.month = 1, ++date.year) : ++date.month;
 	julianDay2 = toJulian(date);
 	return julianDay2 - julianDay1;
 }
@@ -49,51 +50,61 @@ int getDayNameID(gregorianDate date){
 }
 
 
+int calendarLogic(gregorianDate date, int height, int width, int monthLength, int *dayToPrint, int dayNameID){
+	int newWidth = width;
+	bool isBorder = newWidth == 0 || newWidth == CALENDAR_WIDTH - 1;
+	bool isWriteable = newWidth == 2 + 3 * dayNameID && *dayToPrint <= monthLength;
+	bool isAccentuated = date.month == 2 || date.month == 8 || date.month == 12;
+	switch(height){
+		case 0: // First line
+		case 2: // Third line
+		case 10: // Last line
+			printf(isBorder ? "+" : "-");
+			break;
+		case 1: // Second line
+			if(isBorder){ printf("|"); break; }
+			if(newWidth != 2){ printf(" "); break; }
+			// Display month and year
+			printf("%s %d", MONTH_NAMES[date.month - 1], date.year);
+			// Months with an accentuated letter in their name need different spacing
+			newWidth += (int) strlen(MONTH_NAMES[date.month - 1]) + (isAccentuated ? 3 : 4);
+			break;
+		case 3: // Fourth line
+			if(isBorder){ printf("|"); break; }
+			if(newWidth != 2){ printf(" "); break; }
+			// Display names of the days
+			printf("%s", DAYS_NAMES);
+			newWidth += (int) strlen(DAYS_NAMES) - 1;
+			break;
+		default: // All other lines
+			if(isBorder){ printf("|"); break; }
+			if(!isWriteable){ printf(" "); break; }
+			// Display days, if applicable
+			for(; dayNameID < 7; ++dayNameID){
+				*dayToPrint < 10 ? printf(" %d ", *dayToPrint) : printf("%d ", *dayToPrint);
+				++*dayToPrint;
+				newWidth += 3;
+				if(*dayToPrint > monthLength) break;
+			}
+			--newWidth;
+			break;
+	}
+	return newWidth;
+}
+
+
 // Print a calendar page
 void printCalendar(){
 	gregorianDate date = getDate();
 	int monthLength = getMonthLength(date);
 	int dayToPrint = 1;
 	int dayNameID = getDayNameID(date);
+	int height, width;
 	printf("\n");
-	for(int i = 0; i < CALENDAR_HEIGHT; ++i){
-		for(int j = 0; j < CALENDAR_WIDTH; ++j){
-			switch(i){
-				case 0: // First line
-				case 2: // Third line
-				case 10: // Last line
-                    printf(j == 0 || j == CALENDAR_WIDTH - 1 ? "+" : "-");
-					break;
-				case 1: // Second line
-					if(j == 0 || j == CALENDAR_WIDTH - 1) printf("|");
-					else if(j == 2){ // Display month and year
-						printf("%s %d", MONTH_NAMES[date.month - 1], date.year);
-                        j += strlen(MONTH_NAMES[date.month - 1])
-                             // Months with an accentuated letter in their name need different spacing
-                            + (date.month == 2 || date.month == 8 || date.month == 12 ? 3 : 4);
-					} else printf(" ");
-					break;
-				case 3: // Fourth line
-					if(j == 0 || j == CALENDAR_WIDTH - 1) printf("|");
-					else if(j == 2){ // Display names of the days
-						printf("%s", DAYS_NAMES);
-						j += strlen(DAYS_NAMES) - 1;
-					} else printf(" ");
-					break;
-				default: // All other lines
-					if(i > 4) dayNameID = 0; // Reset dayNameID if day 1 has already been printed (= on fifth line)
-					if(j == 0 || j == CALENDAR_WIDTH - 1) printf("|");
-					else if(j == 2 + 3 * dayNameID && dayToPrint <= monthLength){ // Display days, if applicable
-						for(int k = dayNameID; k < 7; ++k){
-							dayToPrint < 10 ? printf(" %d ", dayToPrint) : printf("%d ", dayToPrint);
-							++dayToPrint;
-							j += 3;
-							if(dayToPrint > monthLength) break;
-						}
-						--j;
-					} else printf(" ");
-					break;
-			}
+	for(height = 0; height < CALENDAR_HEIGHT; ++height){
+		if(height == 5) dayNameID = 0; // Reset dayNameID if week 1 has already been printed
+		for(width = 0; width < CALENDAR_WIDTH; ++width){
+			width = calendarLogic(date, height, width, monthLength, &dayToPrint, dayNameID);
 		}
 		printf("\n");
 	}
