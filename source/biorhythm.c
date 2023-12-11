7895f36141e6ec67e61dde65bea086576649090b
Dakuro <51865183+Dakuro@users.noreply.github.com>
2023-12-11 21:27:17 +0100

    Implement some functions

--- source/biorhythm.c
+++ source/biorhythm.c
@@ -1,4 +1,37 @@
+#include <stdio.h>
 #include "../header/functions.h"
 
+// Prompt user for a day, month and year of birth and return their birthdate
+gregorianDate getBirthDate(){
+	gregorianDate birthDate;
+	printf("Entrer une date de naissance (JJ MM AAAA): ");
+	scanf("%d%d%d", &birthDate.day, &birthDate.month, &birthDate.year);
+	return birthDate;
+}
 
 
+// Prompt user for a day, month and year and return the central date of the biorhythm
+gregorianDate getCentralDate(){
+	gregorianDate centralDate;
+	printf("Entrer la date centrale du biorythme (JJ MM AAAA): ");
+	scanf("%d%d%d", &centralDate.day, &centralDate.month, &centralDate.year);
+	return centralDate;
+}
+
+
+// Use the central date to determine 4 dates around it and return them
+bioDates getDates(gregorianDate dateCentral){
+	bioDates dates;
+	dates.dateMinus2 = toGregorian(toJulian(dateCentral) - 14);
+	dates.dateMinus1 = toGregorian(toJulian(dateCentral) - 7);
+	dates.dateCentral = dateCentral;
+	dates.datePlus1 = toGregorian(toJulian(dateCentral) + 7);
+	dates.datePlus2 = toGregorian(toJulian(dateCentral) + 14);
+	return dates;
+}
+
+
+// Print a biorhythm
+void printBiorhythm(){
+
+}
